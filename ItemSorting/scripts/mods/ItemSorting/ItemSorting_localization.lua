1b88a36c9d833e93999f785f200751ac347b67e1
deluxghost <deluxghost@gmail.com>
2023-03-27 09:48:17 +0800

    ItemSorting: add ru localization

--- ItemSorting/scripts/mods/ItemSorting/ItemSorting_localization.lua
+++ ItemSorting/scripts/mods/ItemSorting/ItemSorting_localization.lua
@@ -2,10 +2,12 @@ return {
 	mod_description = {
 		en = "Add extra item sorting methods and remember your sorting method.",
 		["zh-cn"] = "添加额外的物品排序方式，并且记住上次使用的排序方式。",
+		en = "Добавляет дополнительные методы сортировки предметов и запомиает ваш метод сортировки.",
 	},
 	add_extra_sort = {
 		en = "Extra item sorting methods",
 		["zh-cn"] = "额外物品排序方式",
+		en = "Дополнительные методы сортировки предметов",
 	},
 	add_extra_sort_description = {
 		en = "- " .. Localize("loc_inventory_item_grid_sort_title_rarity") .. "\n"
@@ -14,6 +16,7 @@ return {
 	remember_sort_index = {
 		en = "Remember item sorting method",
 		["zh-cn"] = "记住物品排序方式",
+		en = "Запомнить метод сортировки предметов",
 	},
 }
 
