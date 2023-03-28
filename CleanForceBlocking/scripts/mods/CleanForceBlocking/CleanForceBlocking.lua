6deefee4555d73bc52ee55c5a62942624d0b0e94
deluxghost <deluxghost@gmail.com>
2023-03-28 10:05:06 +0800

    CleanForceBlocking: rewrite

--- CleanForceBlocking/scripts/mods/CleanForceBlocking/CleanForceBlocking.lua
+++ CleanForceBlocking/scripts/mods/CleanForceBlocking/CleanForceBlocking.lua
@@ -1,8 +1,12 @@
 local mod = get_mod("CleanForceBlocking")
-local WeaponTemplates = require("scripts/settings/equipment/weapon_templates/weapon_templates")
 local ForceWeaponBlockEffects = require("scripts/extension_systems/visual_loadout/wieldable_slot_scripts/force_weapon_block_effects")
+local ActionDamageTarget = require("scripts/extension_systems/weapon/actions/action_damage_target")
+local ActionPush = require("scripts/extension_systems/weapon/actions/action_push")
 
 local function is_force_sword(template)
+	if not template then
+		return false
+	end
 	if not template.keywords then
 		return false
 	end
@@ -14,42 +18,21 @@ local function is_force_sword(template)
 	return false
 end
 
-mod.on_enabled = function(initial_call)
-	for _, template in pairs(WeaponTemplates) do
-		if is_force_sword(template) and template.actions then
-			local actions = template.actions
-			if actions.action_block then
-				actions.action_block.block_vfx_name = nil
-			end
-			if actions.action_push then
-				actions.action_push.fx = nil
-			end
-			if actions.action_fling_target then
-				actions.action_fling_target.fx = nil
-			end
-		end
-	end
-end
+mod:hook(ForceWeaponBlockEffects, "_update_block_effects", function(func, self, dt)
+	self:_destroy_effects()
+	return
+end)
 
-mod.on_disabled = function(initial_call)
-	for _, template in pairs(WeaponTemplates) do
-		if is_force_sword(template) and template.actions then
-			local actions = template.actions
-			if actions.action_block then
-				actions.action_block.block_vfx_name = "content/fx/particles/weapons/swords/forcesword/psyker_block"
-			end
-			if actions.action_push then
-				actions.action_push.fx = {
-					fx_source = "fx_left_hand",
-					vfx_effect = "content/fx/particles/weapons/swords/forcesword/psyker_parry"
-				}
-			end
-			if actions.action_fling_target then
-				actions.action_fling_target.fx = {
-					fx_source = "fx_left_hand",
-					vfx_effect = "content/fx/particles/weapons/swords/forcesword/psyker_push"
-				}
-			end
-		end
+mod:hook(ActionDamageTarget, "_play_particles", function(func, self)
+	if is_force_sword(self._weapon_template) then
+		return
 	end
-end
+	func(self)
+end)
+
+mod:hook(ActionPush, "_play_push_particles", function(func, self)
+	if is_force_sword(self._weapon_template) then
+		return
+	end
+	func(self)
+end)
