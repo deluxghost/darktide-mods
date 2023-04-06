bc1f7bfa532740ede65dbe4f69907f58a5c3280c
xsSplater <xssplater@gmail.com>
2023-04-06 10:27:16 +0300

    CleanForceBlocking. Update RU localization

--- CleanForceBlocking/scripts/mods/CleanForceBlocking/CleanForceBlocking_localization.lua
+++ CleanForceBlocking/scripts/mods/CleanForceBlocking/CleanForceBlocking_localization.lua
@@ -1,27 +1,31 @@
 return {
+	mod_name = {
+		en = "CleanForceBlocking",
+		ru = "Чистый силовой блок",
+	},
 	mod_description = {
 		en = "Remove blocking and pushing effect of force swords.",
 		["zh-cn"] = "移除力场剑的格挡和推动特效。",
-		ru = "Удаляет эффекты при блокировке и толчках силовыми мечами.",
+		ru = "Удаляет эффекты при блоке и толчках силовыми мечами.",
 	},
 	remove_blocking_effect = {
 		en = "Remove blocking visual effect",
 		["zh-cn"] = "移除格挡视觉效果",
-		ru = "Удалить визуальный эффект при блокировке",
+		ru = "Удаляет визуальный эффект при блоке",
 	},
 	remove_pushing_effect = {
 		en = "Remove pushing visual effect",
 		["zh-cn"] = "移除推动视觉效果",
-		ru = "Удалить визуальный эффект при толчке",
+		ru = "Удаляет визуальный эффект при толчке",
 	},
 	remove_push_attack_effect = {
 		en = "Remove push attack visual effect",
 		["zh-cn"] = "移除推反视觉效果",
-		ru = "Удалить визуальный эффект при атаке после толчка",
+		ru = "Удаляет визуальный эффект при атаке после толчка",
 	},
 	remove_blocking_sound = {
 		en = "Remove blocking sound effect",
 		["zh-cn"] = "移除格挡音效",
-		ru = "Удалить звуковой эффект при блокировке",
+		ru = "Удаляет звуковой эффект при блоке",
 	},
 }
