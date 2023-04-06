c0ada41160d889cb1f2e3bda6a686a51a93409ce
xsSplater <xssplater@gmail.com>
2023-04-06 20:15:38 +0300

    Psykanium Default Difficulty. Added the possibility of localization of the name.

--- PsykaniumDefaultDifficulty/scripts/mods/PsykaniumDefaultDifficulty/PsykaniumDefaultDifficulty_data.lua
+++ PsykaniumDefaultDifficulty/scripts/mods/PsykaniumDefaultDifficulty/PsykaniumDefaultDifficulty_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("PsykaniumDefaultDifficulty")
 
 return {
-	name = "PsykaniumDefaultDifficulty",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 	options = {
