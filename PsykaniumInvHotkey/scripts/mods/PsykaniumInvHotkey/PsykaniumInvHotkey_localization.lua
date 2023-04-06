b2096affe0c4c572de744843d315cfbfc98d4d92
xsSplater <xssplater@gmail.com>
2023-04-06 20:21:06 +0300

    Update PsykaniumInvHotkey_localization.lua

--- PsykaniumInvHotkey/scripts/mods/PsykaniumInvHotkey/PsykaniumInvHotkey_localization.lua
+++ PsykaniumInvHotkey/scripts/mods/PsykaniumInvHotkey/PsykaniumInvHotkey_localization.lua
@@ -1,4 +1,8 @@
 return {
+	mod_name = {
+		en = "PsykaniumInvHotkey",
+		ru = "Клавиша инвентаря для Псайканиума",
+	},
 	mod_description = {
 		en = "You can open inventory in Psykanium by pressing hotkey 'I'.",
 		["zh-cn"] = "你可以在灵能室按 'I' 键打开背包。",
