99dc15c95aad00c9fe21bc076fe0ba7052f2d0ed
xsSplater <xssplater@gmail.com>
2023-04-06 20:53:19 +0300

    Update SpectatorHUD_data.lua

--- SpectatorHUD/scripts/mods/SpectatorHUD/SpectatorHUD_data.lua
+++ SpectatorHUD/scripts/mods/SpectatorHUD/SpectatorHUD_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("SpectatorHUD")
 
 return {
-	name = "SpectatorHUD",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 }
