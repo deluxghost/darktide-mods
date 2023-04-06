573d9e055276b71126950dbcc814c7dbd89eab17
xsSplater <xssplater@gmail.com>
2023-04-06 20:52:56 +0300

    Update SpectatorHUD_localization.lua

--- SpectatorHUD/scripts/mods/SpectatorHUD/SpectatorHUD_localization.lua
+++ SpectatorHUD/scripts/mods/SpectatorHUD/SpectatorHUD_localization.lua
@@ -1,4 +1,8 @@
 return {
+	mod_name = {
+		en = "SpectatorHUD",
+		ru = "Зрительский интерфейс",
+	},
 	mod_description = {
 		en = "You can see the team status HUD after death.",
 		["zh-cn"] = "玩家死亡后可以看到团队状态 HUD。",
