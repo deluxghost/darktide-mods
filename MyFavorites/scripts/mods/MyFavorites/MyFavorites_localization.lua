1610bb10590a88dd180476b7c99326a8209ff536
xsSplater <xssplater@gmail.com>
2023-04-09 15:25:05 +0300

    Update MyFavorites_localization.lua

--- MyFavorites/scripts/mods/MyFavorites/MyFavorites_localization.lua
+++ MyFavorites/scripts/mods/MyFavorites/MyFavorites_localization.lua
@@ -2,17 +2,21 @@ return {
 	mod_name = {
 		en = "My Favorites",
 		["zh-cn"] = "我的收藏",
+		ru = "Моё избранное",
 	},
 	mod_description = {
 		en = "Mark items as favorites, prevent deleting items accidentally.",
 		["zh-cn"] = "将物品标记为收藏，防止不小心删除物品。",
+		ru = "My Favorites - Пометьте предметы как избранные, чтобы предотвратить их случайное удаление.",
 	},
 	unexpected_delete_message = {
 		en = "Unexpected delete action, reopen inventory to recover. Please report this to \"My Favorites\" mod.",
 		["zh-cn"] = "意料之外的删除操作，重新打开背包可恢复。请将此情况报告给“我的收藏”模组。",
+		ru = "Неожиданное действие при удалении, закройте и откройте инвентарь для восстановления. Пожалуйста, сообщите об этом автору мода \"Моё избранное\".",
 	},
 	unexpected_extract_message = {
 		en = "Unexpected extract action, reopen inventory to recover. Please report this to \"My Favorites\" mod.",
 		["zh-cn"] = "意料之外的提取操作，重新打开背包可恢复。请将此情况报告给“我的收藏”模组。",
+		ru = "Неожиданное действие при извлечении, закройте и откройте инвентарь для восстановления. Пожалуйста, сообщите об этом автору мода \"Моё избранное\".",
 	},
 }
