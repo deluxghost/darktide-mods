1b4d16113afdf9586a43d7904a04cb455c6e0424
deluxghost <deluxghost@gmail.com>
2023-04-11 01:01:51 +0800

    add OpenSteamProfile

---
+++ OpenSteamProfile/scripts/mods/OpenSteamProfile/OpenSteamProfile_localization.lua
@@ -0,0 +1,14 @@
+return {
+	mod_name = {
+		en = "Open Steam Profile",
+		["zh-cn"] = "打开 Steam 个人资料",
+	},
+	mod_description = {
+		en = "Let you open another player's Steam profile in the social panel.",
+		["zh-cn"] = "在社交面板打开其他玩家的 Steam 个人资料。",
+    },
+	open_steam_profile_button = {
+		en = "Open Steam Profile",
+		["zh-cn"] = "打开 Steam 个人资料",
+	},
+}
