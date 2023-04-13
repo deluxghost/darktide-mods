fe6cafb2e9cfbb61fdc8af1834abb26167cba6a1
deluxghost <deluxghost@gmail.com>
2023-04-14 01:48:23 +0800

    Reconnect: command desc

--- Reconnect/scripts/mods/Reconnect/Reconnect.lua
+++ Reconnect/scripts/mods/Reconnect/Reconnect.lua
@@ -6,36 +6,36 @@ function mod.on_game_state_changed(status, state_name)
 	if state_name ~= "StateGameplay" then
 		return
 	end
-    if status == "enter" then
+	if status == "enter" then
 		memory.in_game = true
 	else
 		memory.in_game = false
 	end
 end
 
-mod:command("retry", "Reconnect", function()
-    if not memory.in_game then
-        mod:echo(mod:localize("err_not_in_game"))
-        return
-    end
-    if not Managers.multiplayer_session then
-        mod:echo(mod:localize("err_not_in_game"))
-        return
-    end
-    if not Managers.party_immaterium:game_session_in_progress() then
-        mod:echo(mod:localize("err_not_in_game"))
-        return
-    end
-    local players = Managers.player:players()
-    local human = 0
-    for _, player in pairs(players) do
-        if player:is_human_controlled() and player.player_unit then
-            human = human + 1
-        end
-    end
-    if human < 2 then
-        mod:echo(mod:localize("err_only_one_human"))
-        return
-    end
-    Managers.multiplayer_session:_leave("")
+mod:command("retry", mod:localize("retry_description"), function()
+	if not memory.in_game then
+		mod:echo(mod:localize("err_not_in_game"))
+		return
+	end
+	if not Managers.multiplayer_session then
+		mod:echo(mod:localize("err_not_in_game"))
+		return
+	end
+	if not Managers.party_immaterium:game_session_in_progress() then
+		mod:echo(mod:localize("err_not_in_game"))
+		return
+	end
+	local players = Managers.player:players()
+	local human = 0
+	for _, player in pairs(players) do
+		if player:is_human_controlled() and player.player_unit then
+			human = human + 1
+		end
+	end
+	if human < 2 then
+		mod:echo(mod:localize("err_only_one_human"))
+		return
+	end
+	Managers.multiplayer_session:_leave("")
 end)
--- Reconnect/scripts/mods/Reconnect/Reconnect_localization.lua
+++ Reconnect/scripts/mods/Reconnect/Reconnect_localization.lua
@@ -7,6 +7,10 @@ return {
 		en = "Use /retry command to return to main menu so you can reconnect quickly.",
 		["zh-cn"] = "使用 /retry 命令返回主菜单以便快速重新连接。",
 	},
+	retry_description = {
+		en = "Disconnect and return to main menu (reconnectable)",
+		["zh-cn"] = "断开连接并返回主菜单（可重新连接）",
+	},
 	err_not_in_game = {
 		en = "Error: you are not in a game",
 		["zh-cn"] = "错误：你不在一局游戏中",
