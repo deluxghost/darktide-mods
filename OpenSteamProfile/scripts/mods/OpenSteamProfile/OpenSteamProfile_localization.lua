a1688d60ab04c045ddc036b92ec65252c1cedd42
xsSplater <xssplater@gmail.com>
2023-04-18 01:04:55 +0300

    Update OpenSteamProfile_localization.lua

--- OpenSteamProfile/scripts/mods/OpenSteamProfile/OpenSteamProfile_localization.lua
+++ OpenSteamProfile/scripts/mods/OpenSteamProfile/OpenSteamProfile_localization.lua
@@ -2,13 +2,16 @@ return {
 	mod_name = {
 		en = "Open Steam Profile",
 		["zh-cn"] = "打开 Steam 个人资料",
+		ru = "Открыть профиль Steam",
 	},
 	mod_description = {
 		en = "Let you open another player's Steam profile in the social panel.",
 		["zh-cn"] = "在社交面板打开其他玩家的 Steam 个人资料。",
+		ru = "Open Steam Profile - Позволяет открыть Steam-профиль другого игрока в социальной панели.",
     },
 	open_steam_profile_button = {
 		en = "Open Steam Profile",
 		["zh-cn"] = "打开 Steam 个人资料",
+		ru = "Открыть профиль Steam",
 	},
 }
