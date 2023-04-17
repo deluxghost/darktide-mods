e9c74ec2e5c49c6acda61983d5cd3f80ddc666b5
xsSplater <xssplater@gmail.com>
2023-04-18 01:10:18 +0300

    Update Reconnect_localization.lua

--- Reconnect/scripts/mods/Reconnect/Reconnect_localization.lua
+++ Reconnect/scripts/mods/Reconnect/Reconnect_localization.lua
@@ -2,25 +2,31 @@ return {
 	mod_name = {
 		en = "Reconnect",
 		["zh-cn"] = "重新连接",
+		ru = "Переподключение",
 	},
 	mod_description = {
 		en = "Use /retry command to return to main menu so you can reconnect quickly.",
 		["zh-cn"] = "使用 /retry 命令返回主菜单以便快速重新连接。",
+		ru = "Reconnect - Используйте команду /retry для возвращения в главное меню, чтобы вы могли быстро переподключиться к предыдущей сессии.",
 	},
 	retry_description = {
 		en = "Disconnect and return to main menu (reconnectable)",
 		["zh-cn"] = "断开连接并返回主菜单（可重新连接）",
+		ru = "Отключение и возврат в главное меню (с возможностью повторного подключения)",
 	},
 	err_not_in_game = {
 		en = "Error: you are not in a game",
 		["zh-cn"] = "错误：你不在一局游戏中",
+		ru = "Ошибка: вы не в игре",
 	},
 	err_only_one_human = {
 		en = "Error: you are the only human player",
 		["zh-cn"] = "错误：你是唯一的真人玩家",
+		ru = "Ошибка: вы единственный игрок-человек",
 	},
 	retry_keybind = {
 		en = "Keybind for /retry command",
 		["zh-cn"] = "命令 /retry 的快捷键",
+		ru = "Клавиша для команды /retry",
 	},
 }
