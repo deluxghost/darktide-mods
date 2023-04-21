e3a086c74e2aa9cc9312c7891ca9f987658fb70a
xsSplater <xssplater@gmail.com>
2023-04-22 01:29:33 +0300

    Update ChatFix_localization.lua

--- ChatFix/scripts/mods/ChatFix/ChatFix_localization.lua
+++ ChatFix/scripts/mods/ChatFix/ChatFix_localization.lua
@@ -2,29 +2,36 @@ return {
 	mod_name = {
 		en = "Chat Fix",
 		["zh-cn"] = "聊天修复",
+		ru = "Исправление чата",
 	},
 	mod_description = {
 		en = "Some additional chat options.",
 		["zh-cn"] = "添加额外的聊天选项。",
+		ru = "Chat Fix - Добавляет дополнительные параметры для чата.",
 	},
 	chat_join_leave_fix = {
 		en = "Fix join / leave messages",
 		["zh-cn"] = "修复加入/离开消息",
+		ru = "Исправление сообщений о \nприсоединении/выходе",
 	},
 	chat_join_leave_fix_description = {
 		en = "Join / leave messages should be visible without opening the chatbox.",
 		["zh-cn"] = "不打开聊天框也应该显示加入/离开消息。",
+		ru = "Сообщения о присоединении/выходе должны быть видны без открытия окна чата.",
 	},
 	chat_inactivity_timeout = {
 		en = "Chat messages fade timeout",
 		["zh-cn"] = "聊天消息消失时间",
+		ru = "Время исчезновения сообщений",
 	},
 	chat_inactivity_timeout_description = {
 		en = "Hide chat messages after these seconds,\ndefault value of DarkTide is 5 seconds.",
 		["zh-cn"] = "经过多少秒后隐藏最近的聊天消息，\n暗潮的默认值是 5 秒。",
+		ru = "Скрывает сообщения чата по истечении этих секунд, значение DarkTide по умолчанию — 5 секунд.",
 	},
 	chat_scroll_multiplier = {
 		en = "Scroll speed multiplier",
 		["zh-cn"] = "滚动速度倍数",
+		ru = "Множитель скорости прокрутки",
 	},
 }
