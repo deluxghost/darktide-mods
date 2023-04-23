be277d3ce065fe8cc448fa9e8692dca0a6b4a4a1
Zombine <evaseries0404@gmail.com>
2023-04-24 06:11:26 +0900

    Added Japanese localization.

--- QuickLookCard/scripts/mods/QuickLookCard/QuickLookCard_localization.lua
+++ QuickLookCard/scripts/mods/QuickLookCard/QuickLookCard_localization.lua
@@ -6,6 +6,7 @@ return {
 	mod_description = {
 		en = "Check weapon stats in item list quickly.",
 		["zh-cn"] = "在物品列表快速查看武器数据。",
+		ja = "アイテム一覧で素早く武器のステータスを確認できるようになります。",
 	},
 
 	-- To translators: these stat translations must be abbreviations
@@ -13,113 +14,141 @@ return {
 	stat_loc_glossary_term_melee_damage = {
 		en = "MELE", -- Melee Damage
 		["zh-cn"] = "近战", -- 近战伤害
+		ja = "近接", -- 近接ダメージ
 	},
 	stat_loc_stats_display_ammo_stat = {
 		en = "AMMO", -- Ammo
 		["zh-cn"] = "弹药", -- 弹药
+		ja = "弾薬", -- 弾薬
 	},
 	stat_loc_stats_display_ap_stat = {
 		en = "PNT", -- Penetration
 		["zh-cn"] = "穿透", -- 穿透
+		ja = "貫通", -- 貫通力
 	},
 	stat_loc_stats_display_burn_stat = {
 		en = "BURN", -- Burn
 		["zh-cn"] = "燃烧", -- 燃烧
+		ja = "燃焼", -- 燃焼
 	},
 	stat_loc_stats_display_charge_speed = {
 		en = "CHRG", -- Charge Rate
 		["zh-cn"] = "充能", -- 充能速度
+		ja = "溜率", -- チャージ率
 	},
 	stat_loc_stats_display_cleave_damage_stat = {
 		en = "CDMG", -- Cleave Damage
 		["zh-cn"] = "劈伤", -- 劈裂伤害
+		ja = "斬威", -- 斬撃ダメージ
 	},
 	stat_loc_stats_display_cleave_targets_stat = {
 		en = "CTGT", -- Cleave Targets
 		["zh-cn"] = "劈数", -- 劈裂目标数
+		ja = "斬数", -- 斬撃ターゲット数
 	},
 	stat_loc_stats_display_control_stat_melee = {
 		en = "CC", -- Crowd Control
 		["zh-cn"] = "控场", -- 控场
+		ja = "群管", -- 群衆管理
 	},
 	stat_loc_stats_display_control_stat_ranged = {
 		en = "CLTR", -- Collateral
 		["zh-cn"] = "远控", -- 远程控制力
+		ja = "副次", -- コラテラル
 	},
 	stat_loc_stats_display_crit_stat = {
 		en = "CRIT", -- Critical Bonus
 		["zh-cn"] = "暴击", -- 暴击加成
+		ja = "致命", -- クリティカルボーナス
 	},
 	stat_loc_stats_display_damage_stat = {
 		en = "DMG", -- Damage
 		["zh-cn"] = "伤害", -- 伤害
+		ja = "威力", -- ダメージ
 	},
 	stat_loc_stats_display_defense_stat = {
 		en = "DEF", -- Defences
 		["zh-cn"] = "防御", -- 防御
+		ja = "守備", -- 守備力
 	},
 	stat_loc_stats_display_explosion_ap_stat = {
 		en = "PNTB", -- Penetration (Blast)
 		["zh-cn"] = "爆穿", -- 穿透（爆炸）
+		ja = "爆貫", -- 貫通力 (爆発)
 	},
 	stat_loc_stats_display_explosion_damage_stat = {
 		en = "BLST", -- Blast Damage
 		["zh-cn"] = "爆伤", -- 爆炸伤害
+		ja = "爆威", -- 爆発ダメージ
 	},
 	stat_loc_stats_display_explosion_stat = {
 		en = "BLSR", -- Blast Radius
 		["zh-cn"] = "爆距", -- 爆炸范围
+		ja = "爆径", -- 爆破半径
 	},
 	stat_loc_stats_display_finesse_stat = {
 		en = "FNS", -- Finesse
 		["zh-cn"] = "娴熟", -- 武器娴熟
+		ja = "策謀", -- 策謀
 	},
 	stat_loc_stats_display_first_saw_damage = {
 		en = "SRD", -- Shredder
 		["zh-cn"] = "撕裂", -- 撕裂
+		ja = "断裂", -- シュレッダー
 	},
 	stat_loc_stats_display_first_target_stat = {
 		en = "FTGT", -- First Target
 		["zh-cn"] = "首个", -- 首个目标
+		ja = "第一", -- 第一ターゲット
 	},
 	stat_loc_stats_display_flame_size_stat = {
 		en = "CLDR", -- Cloud Radius
 		["zh-cn"] = "火距", -- 火云范围
+		ja = "散布", -- 散布半径
 	},
 	stat_loc_stats_display_heat_management = {
 		en = "TRES", -- Thermal Resistance
 		["zh-cn"] = "热阻", -- 热阻
+		ja = "熱耐", -- 熱耐性
 	},
 	stat_loc_stats_display_mobility_stat = {
 		en = "MOB", -- Mobility
 		["zh-cn"] = "机动", -- 机动性
+		ja = "機動", -- モビリティ
 	},
 	stat_loc_stats_display_power_output = {
 		en = "PWR", -- Power Output
 		["zh-cn"] = "能量", -- 能量输出
+		ja = "電力", -- (電力) 出力
 	},
 	stat_loc_stats_display_power_stat = {
 		en = "STPW", -- Stopping Power
 		["zh-cn"] = "制动", -- 制动能力
+		ja = "抑止", -- 抑止力
 	},
 	stat_loc_stats_display_range_stat = {
 		en = "RNGE", -- Range
 		["zh-cn"] = "范围", -- 攻击范围
+		ja = "範囲", -- 範囲
 	},
 	stat_loc_stats_display_reload_speed_stat = {
 		en = "RLD", -- Reload Speed
 		["zh-cn"] = "装弹", -- 装弹速度
+		ja = "装弾", -- リロード速度
 	},
 	stat_loc_stats_display_stability_stat = {
 		en = "STB", -- Stability
 		["zh-cn"] = "稳定", -- 稳定性
+		ja = "安定", -- 安定性
 	},
 	stat_loc_stats_display_vent_speed = {
 		en = "QUEL", -- Quell Speed
 		["zh-cn"] = "冷却", -- 平息与冷却速度
+		ja = "抑制", -- 速度抑制
 	},
 	stat_loc_stats_display_warp_resist_stat = {
 		en = "WRES", -- Warp Resistance
 		["zh-cn"] = "亚抗", -- 亚空间抗性
+		ja = "歪耐", -- ワープ耐性
 	},
 }
