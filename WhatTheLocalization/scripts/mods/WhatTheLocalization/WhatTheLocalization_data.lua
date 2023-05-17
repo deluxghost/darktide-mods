ddc443cf354df5ffe6e3cd28efd3c6840e759019
unifyh <18213435+unifyh@users.noreply.github.com>
2023-05-17 12:25:07 +0800

    WhatTheLocalization: add toggleable debug mode

--- WhatTheLocalization/scripts/mods/WhatTheLocalization/WhatTheLocalization_data.lua
+++ WhatTheLocalization/scripts/mods/WhatTheLocalization/WhatTheLocalization_data.lua
@@ -4,4 +4,17 @@ return {
 	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
+	options = {
+		widgets = {
+			{
+				setting_id = "toggle_debug_mode",
+				type = "keybind",
+				default_value = {},
+				keybind_global = true,
+				keybind_trigger = "pressed",
+				keybind_type = "function_call",
+				function_name = "toggle_debug_mode",
+			},
+		}
+	}
 }
