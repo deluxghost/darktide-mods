110c6da08303eafd0cce8840c296b2931664a2d9
deluxghost <deluxghost@gmail.com>
2023-05-17 12:54:08 +0800

    WhatTheLocalization: add vraks mk3 zh-cn fix

--- WhatTheLocalization/scripts/mods/WhatTheLocalization/WTL_fix_templates.lua
+++ WhatTheLocalization/scripts/mods/WhatTheLocalization/WTL_fix_templates.lua
@@ -12,6 +12,19 @@ local fix_templates = {
 		end,
 	},
 	{
+		id = "vraks_mk3_headhunter_name_fix_zh_cn",
+		loc_keys = {
+			"loc_autogun_p3_m1",
+		},
+		locales = {
+			"zh-cn",
+		},
+		handle_func = function(locale, value)
+			-- compatible with Mark9
+			return string.gsub(string.gsub(value, "VIII", "III"), "8", "3")
+		end,
+	},
+	{
 		id = "pinning_fire_desc_fix_zh_cn",
 		loc_keys = {
 			"loc_trait_bespoke_stacking_power_bonus_on_staggering_enemies_desc",
