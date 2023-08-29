b822a1606ddba24c2110a1237ca2b11408c92cfc
xsSplater <xssplater@gmail.com>
2023-08-30 01:15:00 +0300

    Update WhatTheLocalization_localization.lua
    TY A LOT!

--- WhatTheLocalization/scripts/mods/WhatTheLocalization/WhatTheLocalization_localization.lua
+++ WhatTheLocalization/scripts/mods/WhatTheLocalization/WhatTheLocalization_localization.lua
@@ -2,34 +2,42 @@ return {
 	mod_name = {
 		en = "What The Localization",
 		["zh-cn"] = "什么鬼翻译",
+		ru = "Что за локализация",
 	},
 	mod_description = {
 		en = "A community-driven localization fix framework and debug tool.",
 		["zh-cn"] = "一个由社区驱动的翻译修复框架和调试工具。",
+		ru = "What The Localization - Созданная сообществом платформа для исправления локализации и инструмент отладки.",
 	},
 	enable_debug_mode = {
 		en = "Debug mode",
 		["zh-cn"] = "调试模式",
+		ru = "Режим отладки",
 	},
 	enable_debug_mode_description = {
 		en = "Show built-in text as localization keys.\nSome text are cached and requires the game to restart.",
 		["zh-cn"] = "以本地化键名显示内置文本。\n部分文本会被缓存，需要重启游戏才能生效。",
+		ru = "Показывать встроенный текст в качестве ключей локализации.\nНекоторый текст кэшируется и требует перезапуска игры.",
 	},
 	toggle_debug_mode = {
 		en = "Keybind: toggle debug mode",
 		["zh-cn"] = "快捷键：切换调试模式",
+		ru = "Привязка клавиш: переключение режима отладки",
 	},
 	message_debug_mode_on = {
 		en = "Localization debug mode: ON",
 		["zh-cn"] = "本地化调试模式：开",
+		ru = "Режим отладки локализации: ВКЛ.",
 	},
 	message_debug_mode_off = {
 		en = "Localization debug mode: OFF",
 		["zh-cn"] = "本地化调试模式：关",
+		ru = "Режим отладки локализации: ВЫКЛ.",
 	},
 	loc_command_description = {
 		en = "Output the value of a localization key.",
 		["zh-cn"] = "输出本地化键名对应的值。",
+		ru = "Выводит значение ключа локализации.",
 	},
 	loc_command_missing_key = {
 		en = "/loc <localization_key>",
@@ -38,10 +46,12 @@ return {
 	loc_command_not_found = {
 		en = "Localization key not found",
 		["zh-cn"] = "本地化键名未找到",
+		ru = "Ключ локализации не найден",
 	},
 	loc_command_output_visualize = {
 		en = "Visualize whitespace characters",
 		["zh-cn"] = "可视化空白字符",
+		ru = "Показывать символы пробелов",
 	},
 	loc_command_output_visualize_description = {
 		en = "Some whitespace characters in the output of /loc command will display as visible icons.\n\n" ..
@@ -50,6 +60,11 @@ return {
 			"\"\\n\" >> \n" ..
 			"\"\\r\" >> ",
 		["zh-cn"] = "以可见图标显示 /loc 命令输出中的一些空白字符。\n\n" ..
+			"\" \" >> \n" ..
+			"\"\\t\" >> \n" ..
+			"\"\\n\" >> \n" ..
+			"\"\\r\" >> ",
+		ru = "Некоторые символы пробелов в выводе команды /loc будут отображаться в виде видимых значков.\n\n" ..
 			"\" \" >> \n" ..
 			"\"\\t\" >> \n" ..
 			"\"\\n\" >> \n" ..
