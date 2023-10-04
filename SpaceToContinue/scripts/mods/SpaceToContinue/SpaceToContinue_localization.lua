be6377797701b16addcd0b668380fb3936f807c2
deluxghost <deluxghost@gmail.com>
2023-10-04 13:12:31 +0800

    add SpaceToContinue

---
+++ SpaceToContinue/scripts/mods/SpaceToContinue/SpaceToContinue_localization.lua
@@ -0,0 +1,10 @@
+return {
+	mod_name = {
+		en = "Space To Continue",
+		["zh-cn"] = "按空格继续",
+	},
+	mod_description = {
+		en = "Revert to the behavior of pressing SPACE to continue at the reward screen.",
+		["zh-cn"] = "回退到在结算界面按空格键继续的行为。",
+	},
+}
