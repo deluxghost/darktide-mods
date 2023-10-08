6ed83b8adf1434ec9a1098a2563838238d4e8039
xsSplater <xssplater@gmail.com>
2023-10-08 18:32:01 +0300

    RU OneMoreTry_localization.lua

--- OneMoreTry/scripts/mods/OneMoreTry/OneMoreTry_localization.lua
+++ OneMoreTry/scripts/mods/OneMoreTry/OneMoreTry_localization.lua
@@ -2,33 +2,41 @@ return {
 	mod_name = {
 		en = "OneMoreTry",
 		["zh-cn"] = "再来一局",
+		ru = "Ещё одна попытка",
 	},
 	mod_description = {
 		en = "Provide /lm command to replay the last mission with your friends.",
 		["zh-cn"] = "提供与好友重玩上次进行的任务的 /lm 命令。",
+		ru = "One More Try - Введите команду /lm, чтобы переиграть последнюю миссию с друзьями.",
 	},
 	lm_command_desc = {
 		en = "Replay the last mission",
 		["zh-cn"] = "重玩上次进行的任务",
+		ru = "Переиграть последнюю миссию",
 	},
 	msg_no_valid_mission = {
 		en = "Error: no valid last mission",
 		["zh-cn"] = "错误：未找到有效的上次任务",
+		ru = "Ошибка: нет действительной последней миссии.",
 	},
 	msg_command_busy = {
 		en = "Error: command is busy",
 		["zh-cn"] = "错误：命令正忙",
+		ru = "Ошибка: команда в обработке",
 	},
 	msg_not_in_hub = {
 		en = "Error: not in Mourningstar",
 		["zh-cn"] = "错误：不在哀星号上",
+		ru = "Ошибка: вы не в Моунингстар",
 	},
 	msg_not_in_party = {
 		en = "Error: not in a Strike Team",
 		["zh-cn"] = "错误：不在打击小队中",
+		ru = "Ошибка: вы не в ударной команде",
 	},
 	msg_on_your_way_to = {
 		en = "On your way to: ",
 		["zh-cn"] = "正在前往：",
+		ru = "Вы отправляетесь в: ",
 	},
 }
