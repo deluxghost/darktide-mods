86aa7fe0629da3bd19c06fa1e891d55b5bf8f348
xsSplater <xssplater@gmail.com>
2023-10-11 21:32:56 +0300

    Update ShareTalents_localization.lua

--- ShareTalents/scripts/mods/ShareTalents/ShareTalents_localization.lua
+++ ShareTalents/scripts/mods/ShareTalents/ShareTalents_localization.lua
@@ -2,45 +2,56 @@ return {
 	mod_name = {
 		en = "Share Talents",
 		["zh-cn"] = "天赋加点分享",
+		ru = "Делитесь билдами",
 	},
 	mod_description = {
 		en = "Share current talents build with /sharetalents command.",
 		["zh-cn"] = "使用 /sharetalents 命令分享当前的天赋加点。",
+		ru = "Share Talents - Поделитесь текущим билдом талантов с помощью команды /sharetalents.",
 	},
 	cmd_share = {
 		en = "Copy current talents build code",
 		["zh-cn"] = "复制当前天赋加点代码到剪贴板",
+		ru = "Скопировать текущий код билда",
 	},
 	cmd_use = {
 		en = "Overwrite talents build code on current profile",
 		["zh-cn"] = "将天赋加点代码覆盖到当前配装",
+		ru = "Переписать билд в текущем профиле",
 	},
 	msg_code_copied = {
 		en = "Talents code copied! This code may expire after a game update",
 		["zh-cn"] = "已复制天赋加点代码！此代码可能会在游戏更新后失效",
+		ru = "Код билда скопирован! Срок действия этого кода может истечь после обновления игры.",
 	},
 	msg_code_applied = {
 		en = "Talents code applied!",
 		["zh-cn"] = "已应用天赋加点代码！",
+		ru = "Код билда применён!",
 	},
 	err_unknown = {
 		en = "Error: unknown error",
 		["zh-cn"] = "错误：未知错误",
+		ru = "Ошибка: неизвестная ошибка",
 	},
 	err_invalid_talents = {
 		en = "Error: current talents invalid",
 		["zh-cn"] = "错误：当前天赋无效",
+		ru = "Ошибка: текущие таланты недействительны",
 	},
 	err_archetype_mismatch = {
 		en = "Error: archetype mismatch",
 		["zh-cn"] = "错误：职业不匹配",
+		ru = "Ошибка: несоответствие архетипа",
 	},
 	err_invalid_code = {
 		en = "Error: talents code invalid or expired",
 		["zh-cn"] = "错误：天赋代码无效或已过期",
+		ru = "Ошибка: код таланта недействителен или срок его действия истёк",
 	},
 	err_argument = {
 		en = "Error: command argument invalid",
 		["zh-cn"] = "错误：命令参数无效",
+		ru = "Ошибка: неверный аргумент команды",
 	},
 }
