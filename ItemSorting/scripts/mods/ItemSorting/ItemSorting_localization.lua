96e1318fe5ba02fb839ea328574bead2758ebae4
xsSplater <xssplater@gmail.com>
2023-10-15 02:04:33 +0300

    Update RU ItemSorting_localization.lua

--- ItemSorting/scripts/mods/ItemSorting/ItemSorting_localization.lua
+++ ItemSorting/scripts/mods/ItemSorting/ItemSorting_localization.lua
@@ -7,12 +7,12 @@ return {
 	mod_description = {
 		en = "Add extra item sorting methods and remember your sorting method.",
 		["zh-cn"] = "添加额外的物品排序方式，并且记住上次使用的排序方式。",
-		ru = "Добавляет дополнительные методы сортировки предметов и запоминает ваш метод сортировки.",
+		ru = "Item Sorting - Добавляет дополнительные методы сортировки предметов и запоминает ваш метод сортировки.",
 	},
 	remember_sort_index = {
 		en = "Remember item sorting method",
 		["zh-cn"] = "记住物品排序方式",
-		ru = "Запомнить метод сортировки предметов",
+		ru = "Запоминать метод сортировки предметов",
 	},
 	group_always_on_top = {
 		en = "Always on top",
@@ -22,10 +22,12 @@ return {
 	always_on_top_curio = {
 		en = "Curios (Store)",
 		["zh-cn"] = "珍品（商店）",
+		ru = "Редкости (Оружейная)",
 	},
 	always_on_top_curio_description = {
 		en = "Curios are always at the top of the store item list.",
 		["zh-cn"] = "珍品总会显示在商店物品列表的顶部。",
+		ru = "Редкости всегда находятся вверху списка товаров в оружейной.",
 	},
 	always_on_top_new = {
 		en = "New items",
@@ -35,7 +37,7 @@ return {
 	always_on_top_new_description = {
 		en = "Items with the \"New Item\" dot are always at the top of the item list.",
 		["zh-cn"] = "有“新物品”圆点的物品总会显示在物品列表的顶部。",
-		ru = "Предметы с точкой \"Новый предмет\" всегда находятся вверху списка предметов.",
+		ru = "Предметы с точкой «Новый предмет» всегда находятся вверху списка предметов.",
 	},
 	always_on_top_equipped = {
 		en = "Equipped items",
@@ -50,30 +52,37 @@ return {
 	always_on_top_myfav = {
 		en = "Favorite items (\"My Favorites\" mod)",
 		["zh-cn"] = "收藏的物品（“我的收藏”模组）",
+		ru = "Избранные предметы (мод «Моё избранное»)",
 	},
 	always_on_top_myfav_description = {
 		en = "With \"My Favorites\" mod, favorite items are always at the top of the item list.",
 		["zh-cn"] = "如果有“我的收藏（My Favorites）”模组，收藏的物品总会显示在物品列表的顶部。",
+		ru = "С модом «Моё избранное» избранные предметы всегда находятся вверху списка предметов.",
 	},
 	group_always_on_top_options = {
 		en = "Always on top options",
 		["zh-cn"] = "总在最上选项",
+		ru = "Опции «Всегда вверху»",
 	},
 	entire_category_on_top = {
 		en = "Top entire blessing category",
 		["zh-cn"] = "祝福分类整体置顶",
+		ru = "Поднять вверх всю категорию «Благословений»",
 	},
 	entire_category_on_top_description = {
 		en = "When sorting by Blessing Category, if an item in the category is topped, the entire category is topped.",
 		["zh-cn"] = "按祝福分类排序时，如果分类内有物品被置顶，则整个分类置顶。",
+		ru = "При сортировке по категории «Благословения», если какой-либо предмет в категории занимает верхнее место, то оказывается наверху вся категория.",
 	},
 	on_top_of_category = {
 		en = "Top inside blessing category",
 		["zh-cn"] = "在祝福分类内置顶",
+		ru = "Наверху в категории «Благословения»",
 	},
 	on_top_of_category_description = {
 		en = "When sorting by Blessing Category, topped items are also topped in their category.",
 		["zh-cn"] = "按祝福分类排序时，置顶的物品也在所属分类内置顶。",
+		ru = "При сортировке по категории «Благословения», лучшие предметы также занимают первое место в своей категории.",
 	},
 	group_custom_sort = {
 		en = "Extra item sorting methods",
@@ -93,7 +102,7 @@ return {
 	custom_sort_category_description = {
 		en = "Weapons fall into same category share same blessing pool.",
 		["zh-cn"] = "相同祝福分类下的武器共享同一个祝福池。",
-		ru = "Оружие попадает в одну категорию, если имеет одинаковые благословения.",
+		ru = "Оружие попадает в одну категорию, если имеет одинаковые Благословения.",
 	},
 	custom_sort_rarity = {
 		en = Localize("loc_inventory_item_grid_sort_title_rarity"),
