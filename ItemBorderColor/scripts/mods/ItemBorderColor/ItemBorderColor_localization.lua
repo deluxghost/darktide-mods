710f2024ba1d3de9a0cde97219ebb401cb819840
xsSplater <xssplater@gmail.com>
2023-10-16 16:10:12 +0300

    RU ItemBorderColor localization

--- ItemBorderColor/scripts/mods/ItemBorderColor/ItemBorderColor_localization.lua
+++ ItemBorderColor/scripts/mods/ItemBorderColor/ItemBorderColor_localization.lua
@@ -2,14 +2,17 @@ return {
 	mod_name = {
 		en = "Item Border Color",
 		["zh-cn"] = "物品边框颜色",
+		ru = "Цвет границы предмета",
 	},
 	mod_description = {
 		en = "Change selected item border color.",
 		["zh-cn"] = "修改已选物品边框颜色。",
+		ru = "Item Border Color - Вы можете изменить цвет границы выбранного предмета.",
 	},
 	opt_selected_color = {
 		en = "Selected item border color",
 		["zh-cn"] = "已选物品边框颜色",
+		ru = "Цвет границы выбранного предмета",
 	},
 	opt_selected_r = {
 		en = "Red",
