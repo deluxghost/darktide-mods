f84c4d476e68202425dadc28392e0c52bddb33e4
xsSplater <xssplater@gmail.com>
2023-10-23 22:30:43 +0300

    RU ShowMeRealWeaponStats localization
    Sorry it took so long. The capacitors in my old monitor died and I had to resolder them.

--- ShowMeRealWeaponStats/scripts/mods/ShowMeRealWeaponStats/ShowMeRealWeaponStats_localization.lua
+++ ShowMeRealWeaponStats/scripts/mods/ShowMeRealWeaponStats/ShowMeRealWeaponStats_localization.lua
@@ -2,29 +2,36 @@ return {
 	mod_name = {
 		en = "Show Me Real Weapon Stats",
 		["zh-cn"] = "给我显示真实的武器数据",
+		ru = "Покажи мне реальные модификаторы",
 	},
 	mod_description = {
 		en = "The real weapon max modifier score is 80. Show me full bar at 80, and real max stats at 80.",
 		["zh-cn"] = "真正的武器修正最大分数是 80。给我显示 80 的满指示条，还有 80 时的「真」最大数据。",
+		ru = "Show Me Real Weapon Stats - Реальное максимальное значение модификатора оружия составляет 80. Так что показывайте мне полную шкалу до 80 и реальные максимальные характеристики до 80.",
 	},
 	max_modifier_score = {
 		en = "Max modifier score",
 		["zh-cn"] = "最大修正分数",
+		ru = "Максимальное значение модификатора",
 	},
 	max_modifier_score_description = {
 		en = "Assuming the max modifier score is this value.",
 		["zh-cn"] = "假定最大修正分数为此数值。",
+		ru = "Максимальное значение модификатора будет стремиться к этому значению, но не меньше того, что есть на оружии.",
 	},
 	show_full_bar = {
 		en = "Show full modifier bar at 80",
 		["zh-cn"] = "在 80 时显示满修正条",
+		ru = "Показывать полоску только до 80",
 	},
 	show_real_max_breakdown = {
 		en = "Show real weapon max stats",
 		["zh-cn"] = "显示真正的武器最大数据",
+		ru = "Показывать реальный максимум",
 	},
 	show_real_max_breakdown_description = {
 		en = "Modify max stats in \"Inspect\" screen, as if the max value is the data at 80 instead of 100.",
 		["zh-cn"] = "修改“检查”界面的最大数据，以 80 而非 100 时的数据为最大值。",
+		ru = "Изменить максимальную характеристику на экране «Проверить», как будто максимальное значение — это 80, а не 100.",
 	},
 }
