c877d18829b987cbeb9ff9b175286d801c9dbf19
xsSplater <xssplater@gmail.com>
2023-12-07 10:42:02 +0300

    RU MergedTalentStats_localization.lua (#6)

--- MergedTalentStats/scripts/mods/MergedTalentStats/MergedTalentStats_localization.lua
+++ MergedTalentStats/scripts/mods/MergedTalentStats/MergedTalentStats_localization.lua
@@ -2,9 +2,11 @@ return {
 	mod_name = {
 		en = "Merged Talent Stats",
 		["zh-cn"] = "合并天赋数据",
+		ru = "Объединённая статистика Талантов",
 	},
 	mod_description = {
 		en = "Merge [Operative Modifier] stats in the talents [Full Build] popup.",
 		["zh-cn"] = "在「完整构筑」弹窗中合并「特工修正」数据。",
+		ru = "Merged Talent Stats - Мод объединяет Модификаторы оперативника в окне Полный билд.",
 	},
 }
