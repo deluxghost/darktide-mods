9a270b9bd7d4a30c6a92398425ee296f596fb018
xsSplater <xssplater@gmail.com>
2023-12-07 11:05:28 +0300

    RU StimmsPickupIcon_localization.lua

--- StimmsPickupIcon/scripts/mods/StimmsPickupIcon/StimmsPickupIcon_localization.lua
+++ StimmsPickupIcon/scripts/mods/StimmsPickupIcon/StimmsPickupIcon_localization.lua
@@ -2,9 +2,11 @@ return {
 	mod_name = {
 		en = "Stimms Pickup Icon",
 		["zh-cn"] = "兴奋剂拾取图标",
+		ru = "Цвет значков стимуляторов",
 	},
 	mod_description = {
 		en = "Set corresponding color for stimms pickup icon.",
 		["zh-cn"] = "给兴奋剂拾取图标设置对应的颜色。",
+		ru = "Stimms Pickup Icon - Установливает соответствующие цвета для значков стимуляторов.",
 	},
 }
