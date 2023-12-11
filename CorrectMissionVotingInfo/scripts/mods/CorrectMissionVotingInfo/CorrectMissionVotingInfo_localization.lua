e2758a068d6b2d184d09298154c56b7b3c02cfad
xsSplater <xssplater@gmail.com>
2023-12-11 22:15:53 +0300

    Update RU CorrectMissionVotingInfo_localization.lua

--- CorrectMissionVotingInfo/scripts/mods/CorrectMissionVotingInfo/CorrectMissionVotingInfo_localization.lua
+++ CorrectMissionVotingInfo/scripts/mods/CorrectMissionVotingInfo/CorrectMissionVotingInfo_localization.lua
@@ -2,9 +2,11 @@ return {
 	mod_name = {
 		en = "Correct Mission Voting Info",
 		["zh-cn"] = "正确的任务投票信息",
+		ru = "Исправление информации о миссии",
 	},
 	mod_description = {
 		en = "When voting for a mission, shows correct auric info and total rewards.",
 		["zh-cn"] = "在任务投票时，显示正确的金级信息和总奖励。",
+		ru = "Correct Mission Voting Info - При голосовании за миссию отображается правильная информация о миссии и сумме награды.",
 	},
 }
