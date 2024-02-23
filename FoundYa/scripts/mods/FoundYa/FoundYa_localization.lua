ffea7f31122cededcd33d7850be51ab20ab07125
xsSplater <xssplater@gmail.com>
2024-02-23 20:47:51 +0300

    Update RU FoundYa_localization.lua

--- FoundYa/scripts/mods/FoundYa/FoundYa_localization.lua
+++ FoundYa/scripts/mods/FoundYa/FoundYa_localization.lua
@@ -2,67 +2,82 @@ return {
 	mod_name = {
 		en = "Found Ya",
 		["zh-cn"] = "发现物品",
+		ru = "Нашёл!",
 	},
 	mod_description = {
 		en = "Adjust the view/tag distance of item interaction icons.",
 		["zh-cn"] = "调整物品交互图标的可视与标记距离。",
-		ru = "Регулирует расстояние видимости/пометки значков взаимодействия с предметами.",
+		ru = "Found Ya - Регулирует расстояние видимости/пометки значков взаимодействия с предметами.",
 	},
 	max_distance = {
 		en = "Max view distance (default: 15m)",
 		["zh-cn"] = "最大可视距离（默认：15 米）",
+		ru = "Максимальное расстояние видимости (по умолчанию: 15 м)",
 	},
 	alter_icons = {
 		en = "Alternative icons",
 		["zh-cn"] = "改版图标",
+		ru = "Альтернативные значки",
 	},
 	alter_chest_icon = {
 		en = "Alternative chests icon",
 		["zh-cn"] = "改版箱子图标",
+		ru = "Альтернативный значок ящиков",
 	},
 	alter_luggable_icon = {
 		en = "Alternative luggables icon",
 		["zh-cn"] = "改版搬运物图标",
+		ru = "Альтернативный значок переносимого",
 	},
 	alter_large_ammo_icon = {
 		en = "Alternative large ammo clips icon",
 		["zh-cn"] = "改版大弹药包图标",
+		ru = "Альтернативный значок больших обойм с боеприпасами",
 	},
 	max_distance_material = {
 		en = "Forge materials",
 		["zh-cn"] = "锻造材料",
+		ru = "Материалы кузницы",
 	},
 	max_distance_book = {
 		en = "Secondary objectives (scriptures and grimoires)",
 		["zh-cn"] = "次要目标（圣经和魔法书）",
+		ru = "Второстепенные задачи (писания и гримуары)",
 	},
 	max_distance_supply = {
 		en = "Supplies",
 		["zh-cn"] = "补给品",
+		ru = "Припасы",
 	},
 	max_distance_chest = {
 		en = "Chests",
 		["zh-cn"] = "箱子",
+		ru = "Ящики",
 	},
 	max_distance_luggable = {
 		en = "Luggables",
 		["zh-cn"] = "搬运物",
+		ru = "Переносимые",
 	},
 	max_distance_button = {
 		en = "Buttons",
 		["zh-cn"] = "按钮",
+		ru = "Кнопки",
 	},
 	max_distance_station = {
 		en = "Healing stations",
 		["zh-cn"] = "医疗站",
+		ru = "Медстанции",
 	},
 	max_distance_vendor = {
 		en = "Vendors",
 		["zh-cn"] = "商店图标",
+		ru = "Торговцы",
 	},
 	max_distance_unknown = {
 		en = "Other supported interactables",
 		["zh-cn"] = "其他支持的交互项",
+		ru = "Другие поддерживаемые интерактивные объекты",
 	},
 	max_tag_distance = {
 		en = "Max tag distance",
