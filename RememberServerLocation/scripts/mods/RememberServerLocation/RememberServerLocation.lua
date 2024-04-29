726e213f54d2791e120d53263faf39ee2d2a561c
deluxghost <deluxghost@gmail.com>
2024-04-30 02:08:07 +0800

    RememberServerLocation: fix backend not initialized

--- RememberServerLocation/scripts/mods/RememberServerLocation/RememberServerLocation.lua
+++ RememberServerLocation/scripts/mods/RememberServerLocation/RememberServerLocation.lua
@@ -3,63 +3,76 @@ local MissionBoardView = require("scripts/ui/views/mission_board_view/mission_bo
 local StoryMissionPlayView = require("scripts/ui/views/story_mission_play_view/story_mission_play_view")
 local BackendUtilities = require("scripts/foundation/managers/backend/utilities/backend_utilities")
 local Promise = require("scripts/foundation/utilities/promise")
+local RegionLocalizationMappings = require("scripts/settings/backend/region_localization")
 
 mod.memory = mod:persistent_table("memory")
 
 local function save_region()
 	if BackendUtilities.prefered_mission_region ~= "" then
+		mod:info("save server region " .. (BackendUtilities.prefered_mission_region or ""))
 		mod:set("saved_server_location", BackendUtilities.prefered_mission_region)
 	end
 end
 
 local function fetch_regions()
-	local region_promise = Managers.backend.interfaces.region_latency:get_region_latencies()
-
-	region_promise:next(function (regions_data)
-		local prefered_region_promise = nil
-
-		if BackendUtilities.prefered_mission_region == "" then
-			prefered_region_promise = Managers.backend.interfaces.region_latency:get_preferred_reef()
-		else
-			prefered_region_promise = Promise.resolved()
-		end
-
-		prefered_region_promise:next(function (prefered_region)
-			local prefered_mission_region = BackendUtilities.prefered_mission_region ~= "" and BackendUtilities.prefered_mission_region or prefered_region or regions_data[1].reefs[1]
-			local regions_latency = Managers.backend.interfaces.region_latency:get_reef_info_based_on_region_latencies(regions_data)
-			mod.memory.regions_latency = regions_latency
-			if mod:is_enabled() then
-				local saved_server_location = mod:get("saved_server_location")
-				if saved_server_location then
-					if regions_latency[saved_server_location] then
-						prefered_mission_region = saved_server_location
+	Promise.until_true(function()
+		return Managers.backend:authenticated()
+	end):next(function()
+		mod:info("start fetching regions")
+		local region_promise = Managers.backend.interfaces.region_latency:get_region_latencies()
+		region_promise:next(function(regions_data)
+			local prefered_region_promise = Managers.backend.interfaces.region_latency:get_preferred_reef()
+			prefered_region_promise:next(function (prefered_region)
+				local prefered_mission_region = prefered_region or regions_data[1].reefs[1] or ""
+				local regions_latency = Managers.backend.interfaces.region_latency:get_reef_info_based_on_region_latencies(regions_data)
+				mod:dump(regions_latency, "rsl_regions_latency", 3)
+				mod.memory.regions_latency = regions_latency
+				if mod:is_enabled() then
+					local saved_server_location = mod:get("saved_server_location")
+					if saved_server_location then
+						if regions_latency[saved_server_location] then
+							prefered_mission_region = saved_server_location
+							mod:info("load server region " .. saved_server_location)
+						end
 					end
+					BackendUtilities.prefered_mission_region = prefered_mission_region
+					mod:info("apply server region " .. prefered_mission_region)
+					save_region()
 				end
-				BackendUtilities.prefered_mission_region = prefered_mission_region
-				save_region()
-			end
+			end)
 		end)
 	end)
 end
 
 mod.on_all_mods_loaded = function()
+	local saved_server_location = mod:get("saved_server_location") or ""
+	if not RegionLocalizationMappings[saved_server_location] then
+		mod:info("clear unknown server region " .. saved_server_location)
+		saved_server_location = ""
+		mod:set("saved_server_location", saved_server_location)
+	end
+	BackendUtilities.prefered_mission_region = saved_server_location
 	if not mod.memory.regions_latency then
 		fetch_regions()
 	end
 end
 
 mod:hook_safe(MissionBoardView, "on_exit", function(self)
+	mod:info("exit mission board")
 	save_region()
 end)
 
 mod:hook_safe(MissionBoardView, "_callback_close_options", function(self)
+	mod:info("exit mission board options")
 	save_region()
 end)
 
 mod:hook_safe(StoryMissionPlayView, "on_exit", function(self)
+	mod:info("exit story view")
 	save_region()
 end)
 
 mod:hook_safe(StoryMissionPlayView, "_callback_close_options", function(self)
+	mod:info("exit story view options")
 	save_region()
 end)
