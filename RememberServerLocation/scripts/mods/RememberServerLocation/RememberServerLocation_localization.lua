5dcc101d400e87a7be6cc4a0da146c29b94ea757
xsSplater <xssplater@gmail.com>
2024-05-13 13:27:33 +0300

    Update RU RememberServerLocation_localization.lua

--- RememberServerLocation/scripts/mods/RememberServerLocation/RememberServerLocation_localization.lua
+++ RememberServerLocation/scripts/mods/RememberServerLocation/RememberServerLocation_localization.lua
@@ -2,9 +2,11 @@ return {
 	mod_name = {
 		en = "Remember Server Location",
 		["zh-cn"] = "记住服务器位置",
+		ru = "Помни расположение сервера",
 	},
 	mod_description = {
 		en = "Remember matchmaking server location option.",
 		["zh-cn"] = "记住匹配服务器位置选项。",
+		ru = "Remember Server Location - Запоминает расположение сервера подбора Миссии в опциях.",
 	},
 }
