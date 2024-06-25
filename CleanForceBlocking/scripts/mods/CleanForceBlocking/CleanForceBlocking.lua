9f945b7c094d38e671752021cb8a81e099c93f7d
deluxghost <deluxghost@gmail.com>
2024-06-26 02:31:35 +0800

    CleanForceBlocking: fix for game update

--- CleanForceBlocking/scripts/mods/CleanForceBlocking/CleanForceBlocking.lua
+++ CleanForceBlocking/scripts/mods/CleanForceBlocking/CleanForceBlocking.lua
@@ -1,5 +1,4 @@
 local mod = get_mod("CleanForceBlocking")
-local PlayerUnitFxExtension = require("scripts/extension_systems/fx/player_unit_fx_extension")
 local ForceWeaponBlockEffects = require("scripts/extension_systems/visual_loadout/wieldable_slot_scripts/force_weapon_block_effects")
 
 local function is_force_sword(template)
@@ -48,28 +47,59 @@ mod:hook_require("scripts/extension_systems/weapon/actions/action_push", functio
 	end)
 end)
 
-mod:hook(PlayerUnitFxExtension, "_trigger_looping_wwise_event", function(func, self, sound_alias, optional_source_name)
-	if not mod:get("remove_blocking_sound") then
+mod.on_all_mods_loaded = function ()
+	mod:hook(CLASS.PlayerUnitFxExtension, "_trigger_looping_wwise_event", function(func, self, sound_alias, optional_source_name)
+		if not mod:get("remove_blocking_sound") then
+			return func(self, sound_alias, optional_source_name)
+		end
+		if sound_alias == "block_loop" and optional_source_name == "slot_primary_block" then
+			return
+		end
 		return func(self, sound_alias, optional_source_name)
-	end
-	if sound_alias == "block_loop" and optional_source_name == "slot_primary_block" then
-		return
-	end
-	return func(self, sound_alias, optional_source_name)
-end)
+	end)
 
-mod:hook(PlayerUnitFxExtension, "rpc_stop_looping_player_sound", function(func, self, channel_id, game_object_id, sound_alias_id)
-	if not mod:get("remove_blocking_sound") then
-		return func(self, channel_id, game_object_id, sound_alias_id)
-	end
-	local sound_alias = NetworkLookup.player_character_looping_sound_aliases[sound_alias_id]
-	local data = self._looping_sounds[sound_alias]
-	if not data then
-		return func(self, channel_id, game_object_id, sound_alias_id)
-	end
+	mod:hook(CLASS.PlayerUnitFxExtension, "rpc_stop_looping_player_sound", function(func, self, channel_id, game_object_id, sound_alias_id)
+		if not mod:get("remove_blocking_sound") then
+			return func(self, channel_id, game_object_id, sound_alias_id)
+		end
+		local sound_alias = NetworkLookup.player_character_looping_sound_aliases[sound_alias_id]
+		local data = self._looping_sounds[sound_alias]
+		if not data then
+			return func(self, channel_id, game_object_id, sound_alias_id)
+		end
 
-	if sound_alias == "block_loop" and (not data.is_playing) then
-		return
-	end
-	return func(self, channel_id, game_object_id, sound_alias_id)
-end)
+		if sound_alias == "block_loop" and (not data.is_playing) then
+			return
+		end
+		return func(self, channel_id, game_object_id, sound_alias_id)
+	end)
+end
+-- mod:hook_require("scripts/extension_systems/fx/player_unit_fx_extension", function(PlayerUnitFxExtension)
+-- 	mod:hook(PlayerUnitFxExtension, "_trigger_looping_wwise_event", function(func, self, sound_alias, optional_source_name)
+-- 		if not mod:get("remove_blocking_sound") then
+-- 			return func(self, sound_alias, optional_source_name)
+-- 		end
+-- 		if sound_alias == "block_loop" and optional_source_name == "slot_primary_block" then
+-- 			return
+-- 		end
+-- 		return func(self, sound_alias, optional_source_name)
+-- 	end)
+-- end)
+
+-- mod:hook_require("scripts/extension_systems/fx/player_unit_fx_extension", function(PlayerUnitFxExtension)
+-- 	mod:hook(PlayerUnitFxExtension, "rpc_stop_looping_player_sound", function(func, self, channel_id, game_object_id, sound_alias_id)
+-- 		if not mod:get("remove_blocking_sound") then
+-- 			return func(self, channel_id, game_object_id, sound_alias_id)
+-- 		end
+-- 		local sound_alias = NetworkLookup.player_character_looping_sound_aliases[sound_alias_id]
+-- 		local data = self._looping_sounds[sound_alias]
+-- 		if not data then
+-- 			return func(self, channel_id, game_object_id, sound_alias_id)
+-- 		end
+
+-- 		if sound_alias == "block_loop" and (not data.is_playing) then
+-- 			return
+-- 		end
+-- 		return func(self, channel_id, game_object_id, sound_alias_id)
+-- 	end)
+-- end)
