7923ee3cf93c4667f546b357f33f66524494653f
xsSplater <xssplater@gmail.com>
2024-06-28 22:53:05 +0300

    Update RU SoloPlay and QuickLookCard (#48)
    * Update RU SoloPlay_localization.lua
    
    * Update RU QuickLookCard_localization.lua
    
    * Update SoloPlay_localization.lua

--- SoloPlay/scripts/mods/SoloPlay/SoloPlay_localization.lua
+++ SoloPlay/scripts/mods/SoloPlay/SoloPlay_localization.lua
@@ -5,70 +5,87 @@ local loc = {
 	mod_name = {
 		en = "Solo Play",
 		["zh-cn"] = "单人游戏",
+		ru = "Игра в соло",
 	},
 	mod_description = {
 		en = "Play offline solo mission with /solo command. You won't get any progression or reward.",
 		["zh-cn"] = "输入 /solo 命令玩离线单人任务。不会获得任何进度或奖励。",
+		ru = "Solo Play - Пройдите миссию в одиночку в офлайн режиме с помощью команды чата /solo. Вы не получите никакого прогресса или награды.",
 	},
 	solo_command_desc = {
 		en = "Play offline solo mission",
 		["zh-cn"] = "玩离线单人任务",
+		ru = "Пройдите миссию в одиночку в офлайн режиме",
 	},
 	msg_not_in_hub_or_mission = {
 		en = "Error: not in Mourningstar, Psykhanium or Solo Play",
 		["zh-cn"] = "错误：不在哀星号上、灵能室内或单人任务中",
+		ru = "Ошибка: не в Моунингстар, Псайкаинуме или в Соло миссии.",
 	},
 	msg_not_valid_mission_giver = {
 		en = "Error: invalid mission giver for selected mission, valid options: ",
 		["zh-cn"] = "错误：任务发布者对所选任务无效，有效的选项：",
+		ru = "Ошибка: неверный идентификатор для выбранной миссии, допустимые варианты: ",
 	},
 	msg_starting_soloplay = {
 		en = "Starting solo mission...",
 		["zh-cn"] = "正在开始单人任务...",
+		ru = "Начинаю миссию в соло...",
 	},
 	group_mission_settings = {
 		en = "Mission settings",
 		["zh-cn"] = "任务设置",
+		ru = "Настройки миссии",
 	},
 	choose_mission = {
 		en = "Choose mission",
 		["zh-cn"] = "选择任务",
+		ru = "Выберите миссию",
 	},
 	choose_difficulty = {
 		en = "Choose difficulty",
 		["zh-cn"] = "选择难度",
+		ru = "Выберите сложность",
 	},
 	choose_side_mission = {
 		en = "Choose side mission",
 		["zh-cn"] = "选择次要目标",
+		ru = "Выбрать побочную миссию",
 	},
 	choose_side_mission_description = {
 		en = "Note: in specific missions, some options may not be supported, not work, or cause crashes.",
 		["zh-cn"] = "注意：在特定任务下，某些选项可能不受支持、不生效或导致崩溃。",
+		ru = "Примечание: в определённых миссиях некоторые опции могут не поддерживаться, не работать или вызывать сбои.",
 	},
 	choose_circumstance = {
 		en = "Choose special condition",
 		["zh-cn"] = "选择特殊状况",
+		ru = "Выберите специальное условие",
 	},
 	choose_circumstance_description = {
 		en = "Note: in specific missions, some options may not be supported, not work, or cause crashes.",
 		["zh-cn"] = "注意：在特定任务下，某些选项可能不受支持、不生效或导致崩溃。",
+		ru = "Примечание: в определённых миссиях некоторые опции могут не поддерживаться, не работать или вызывать сбои.",
 	},
 	choose_mission_giver = {
 		en = "Choose mission giver",
 		["zh-cn"] = "选择任务发布者",
+		ru = "Выберите дающего миссию",
 	},
 	choose_mission_giver_description = {
 		en = "Note: in specific missions, some options may not be supported, not work, or cause crashes.",
 		["zh-cn"] = "注意：在特定任务下，某些选项可能不受支持、不生效或导致崩溃。",
+		ru = "Примечание: в определённых миссиях некоторые опции могут не поддерживаться, не работать или вызывать сбои.",
 	},
 	default_text = {
 		en = "Default",
 		["zh-cn"] = "默认",
+		ru = "По умолчанию",
 	},
 	default_text_none = {
 		en = "None",
 		["zh-cn"] = "无",
+		ru = "Нет",
 	},
 	format_auric = {
 		en = Localize("loc_mission_board_type_auric") .. " %s",
@@ -77,20 +94,25 @@ local loc = {
 	group_modifiers = {
 		en = "Modifiers",
 		["zh-cn"] = "修改项",
+		ru = "Модификаторы",
 	},
 	friendly_fire_enabled = {
 		en = "Friendly fire",
 		["zh-cn"] = "友军伤害",
+		ru = "Огонь по своим",
 	},
 	random_side_mission_seed = {
 		en = "Random side mission location",
 		["zh-cn"] = "随机次要目标位置",
+		ru = "Случайные места побочной миссии",
 	},
 	random_side_mission_seed_description = {
 		en = "Currently, side mission pickup spawn locations are locked and only changed on a weekly cadence.\n" ..
 			"This option enables the old random location generator in Solo Play.",
 		["zh-cn"] = "目前，次要目标的生成位置是固定的，仅会每周刷新一次。\n" ..
 			"此选项会在单人游戏中启用旧版的随机位置生成器。",
+		ru = "В настоящее время места появления побочных миссий заблокированы и меняются только еженедельно.\n" ..
+			"Эта опция включает старый генератор случайных мест в соло игре.",
 	},
 }
 
