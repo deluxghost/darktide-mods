f6ccacc12a638a093b5c0ffbc1632a49f116ce41
deluxghost <deluxghost@gmail.com>
2024-07-22 16:01:26 +0800

    IME_Enable: update description

--- IME_Enable/scripts/mods/IME_Enable/IME_Enable_localization.lua
+++ IME_Enable/scripts/mods/IME_Enable/IME_Enable_localization.lua
@@ -5,8 +5,8 @@ return {
 	},
 	mod_description = {
 		en = "Enable Windows Input Method for in-game text boxes. Useful for such as CJK languages players.\n\n" ..
-			"Some input methods themselves may not compatible with Darktide (e.g. Weasel), which is not fixable by this mod.",
+			"Some input methods themselves may not compatible with Darktide, which is not fixable by this mod.",
 		["zh-cn"] = "为游戏内文本框启用 Windows 输入法支持。适用于中日韩等语言的玩家。\n\n"..
-			"一些输入法自身不兼容暗潮（例如小狼毫），此模组无法解决此类问题。",
+			"一些输入法自身不兼容暗潮，此模组无法解决此类问题。",
 	},
 }
