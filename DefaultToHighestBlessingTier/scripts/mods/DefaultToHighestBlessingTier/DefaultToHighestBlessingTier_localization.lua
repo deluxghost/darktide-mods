0bb5c18992559a38e6682ad20939754bc03c037e
xsSplater - СанСаныч <xssplater@gmail.com>
2024-09-29 20:58:04 +0300

    RU DefaultToHighestBlessingTier_localization.lua

--- DefaultToHighestBlessingTier/scripts/mods/DefaultToHighestBlessingTier/DefaultToHighestBlessingTier_localization.lua
+++ DefaultToHighestBlessingTier/scripts/mods/DefaultToHighestBlessingTier/DefaultToHighestBlessingTier_localization.lua
@@ -2,9 +2,11 @@ return {
 	mod_name = {
 		en = "Default To Highest Blessing Tier",
 		["zh-cn"] = "默认最高祝福等级",
+		ru = "Благословения высшего уровня по умолчанию",
 	},
 	mod_description = {
 		en = "Automatically switch to the highest unlocked blessing tier in Re-Bless page.",
 		["zh-cn"] = "在重新祝福页面，自动切换到已解锁的最高祝福等级。",
+		ru = "Default To Highest Blessing Tier - Автоматически переключает на наивысший разблокированный уровень благословения на странице смены благословений.",
 	},
 }
