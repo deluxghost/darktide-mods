318b09851515cc4a04340b4939f3b45d23b0a3d7
xsSplater - СанСаныч <xssplater@gmail.com>
2024-10-04 13:02:50 +0300

    Update RU WarpUnboundTimer_localization.lua (#11)

--- WarpUnboundTimer/scripts/mods/WarpUnboundTimer/WarpUnboundTimer_localization.lua
+++ WarpUnboundTimer/scripts/mods/WarpUnboundTimer/WarpUnboundTimer_localization.lua
@@ -2,9 +2,11 @@ return {
 	mod_name = {
 		en = "Warp Unbound Timer",
 		["zh-cn"] = "亚空间解放计时器",
+		ru = "Таймер Искажения варпа",
 	},
 	mod_description = {
 		en = "Show a countdown timer of psyker's Warp Unbound talent.",
 		["zh-cn"] = "显示灵能者亚空间解放天赋的倒计时。",
+		ru = "Warp Unbound Timer - Показывает таймер обратного отсчёта таланта псайкера «Искажение варпа».",
 	},
 }
