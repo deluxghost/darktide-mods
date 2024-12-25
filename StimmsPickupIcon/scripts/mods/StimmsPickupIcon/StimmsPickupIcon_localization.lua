f455502837c2574778038dbf333a689641e32c60
xsSplater - СанСаныч <xssplater@gmail.com>
2024-12-25 18:47:04 +0300

    Update StimmsPickupIcon_localization.lua

--- StimmsPickupIcon/scripts/mods/StimmsPickupIcon/StimmsPickupIcon_localization.lua
+++ StimmsPickupIcon/scripts/mods/StimmsPickupIcon/StimmsPickupIcon_localization.lua
@@ -12,17 +12,21 @@ return {
 	show_pickup_color = {
 		en = "Show colorful pickup icons",
 		["zh-cn"] = "显示彩色拾取图标",
+		ru = "Показать цветные значки поднимаемых",
 	},
 	show_team_panel_color = {
 		en = "Show colorful icons in team HUD",
 		["zh-cn"] = "在团队 HUD 中显示彩色图标",
+		ru = "Показывать цветные значки в командном интерфейсе",
 	},
 	show_self_weapon_color = {
 		en = "Show colorful icons in weapon HUD",
 		["zh-cn"] = "在武器 HUD 中显示彩色图标",
+		ru = "Показывать цветные иконки в интерфейсе оружия",
 	},
 	use_recolor_stimms_mod = {
 		en = "Use color settings of RecolorStimms mod",
 		["zh-cn"] = "使用 RecolorStimms 模组的颜色设置",
+		ru = "Использовать настройки цвета мода «Recolor Stimms»",
 	},
 }
