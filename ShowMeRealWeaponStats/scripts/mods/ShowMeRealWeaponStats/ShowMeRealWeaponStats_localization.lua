5a3e96427b0bf4b1ad0685e95652745a265d22a1
xsSplater - СанСаныч <xssplater@gmail.com>
2024-12-25 19:29:11 +0300

    Update RU: SoloPlay, ShowMeRealWeaponStats and StimmsPickupIcon (#58)
    * Update SoloPlay_localization.lua
    
    * Update ShowMeRealWeaponStats_localization.lua
    
    * Update StimmsPickupIcon_localization.lua

--- ShowMeRealWeaponStats/scripts/mods/ShowMeRealWeaponStats/ShowMeRealWeaponStats_localization.lua
+++ ShowMeRealWeaponStats/scripts/mods/ShowMeRealWeaponStats/ShowMeRealWeaponStats_localization.lua
@@ -7,65 +7,81 @@ return {
 	mod_description = {
 		en = "Shows real weapon stats with a maximum value of 80.",
 		["zh-cn"] = "显示最大值为 80 时的「真实」武器数据。",
+		ru = "Show Me Real Weapon Stats - Показывает максимальное значение модификаторов оружия до 80, как оно есть.",
 	},
 	group_value_override = {
 		en = "Threshold value override",
 		["zh-cn"] = "阈值覆盖设置",
+		ru = "Переопределение порогового значения",
 	},
 	max_modifier_score = {
 		en = "Max stats value",
 		["zh-cn"] = "五维属性最大值",
+		ru = "Максимальное значение модификаторов",
 	},
 	max_dump_modifier_score = {
 		en = "Max dump stats value",
 		["zh-cn"] = "五维短板属性最大值",
+		ru = "Максимальное значение худшего модификатора",
 	},
 	group_inspect_view = {
 		en = "Weapon inspect view",
 		["zh-cn"] = "武器检视界面",
+		ru = "Осмотр оружия",
 	},
 	current_substats_display_mode = {
 		en = "Current substats (gold numbers) display mode",
 		["zh-cn"] = "当前详细数据（金色数字）显示模式",
+		ru = "Режим отображения текущих модификаторов (золотые числа)",
 	},
 	current_substats_display_mode_default = {
 		en = "Current value",
 		["zh-cn"] = "当前值",
+		ru = "Текущее значение",
 	},
 	current_substats_display_mode_potential = {
 		en = "Current value / Potential value",
 		["zh-cn"] = "当前值 / 潜力值",
+		ru = "Текущее значение / Потенциальное значение",
 	},
 	substats_range_display_mode = {
 		en = "Substats range (grey numbers) display mode",
 		["zh-cn"] = "详细数据范围（灰色数字）显示模式",
+		ru = "Режим отображения диапазона текущих модификаторов (серые числа)",
 	},
 	substats_range_display_mode_description = {
 		en = "\"Threshold value override\" options will affect the behavior of this option.",
 		["zh-cn"] = "“阈值覆盖设置”会影响此选项的效果。",
+		ru = "Параметр «Переопределение порогового значения» повлияют на поведение этой опции.",
 	},
 	substats_range_display_mode_0_100 = {
 		en = "[ Value @ 0%% | Value @ 100%% ]",
 		["zh-cn"] = "[ 0%% 数值 | 100%% 数值 ]",
+		ru = "[ Значение @ 0%% | Значение @ 100%% ]",
 	},
 	substats_range_display_mode_0_80 = {
 		en = "[ Value @ 0%% | Value @ 80%% ]",
 		["zh-cn"] = "[ 0%% 数值 | 80%% 数值 ]",
+		ru = "[ Значение @ 0%% | Значение @ 80%% ]",
 	},
 	substats_range_display_mode_60_80 = {
 		en = "[ Value @ 60%% | Value @ 80%% ]",
 		["zh-cn"] = "[ 60%% 数值 | 80%% 数值 ]",
+		ru = "[ Значение @ 60%% | Значение @ 80%% ]",
 	},
 	group_weapon_card = {
 		en = "Weapon info card",
 		["zh-cn"] = "武器信息卡片",
+		ru = "Карточка оружия",
 	},
 	show_full_bar = {
 		en = "Full bar at 80 stats",
 		["zh-cn"] = "五维 80 显示满数据条",
+		ru = "Полная полоска на значении 80",
 	},
 	show_full_bar_description = {
 		en = "Fills the stats bar when they reach 80.\n\n\"Threshold value override\" options will affect the behavior of this option.",
 		["zh-cn"] = "五维达到 80 时，显示满数据条。\n\n“阈值覆盖设置”会影响此选项的效果。",
+		ru = "Заполняет полоску модификатора, когда она достигает 80.\n\nПараметр «Переопределение порогового значения» влияют на поведение этого параметра.",
 	},
 }
