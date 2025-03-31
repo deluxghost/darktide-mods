522e6b603cc9e627de88962b7d83a43085a196d1
SyuanTsai <carsun00@Gmail.com>
2025-04-01 02:26:14 +0800

    Fix

--- StimmsPickupIcon/scripts/mods/StimmsPickupIcon/StimmsPickupIcon_localization.lua
+++ StimmsPickupIcon/scripts/mods/StimmsPickupIcon/StimmsPickupIcon_localization.lua
@@ -2,37 +2,37 @@ return {
 	mod_name = {
 		en = "Stimms Pickup Icon",
 		["zh-cn"] = "兴奋剂拾取图标",
-        ["zh-tw"] = "興奮劑拾取圖示",
+		["zh-tw"] = "興奮劑拾取圖示",
 		ru = "Цвет значков стимуляторов",
 	},
 	mod_description = {
 		en = "Set corresponding color for stimms icons.",
 		["zh-cn"] = "给兴奋剂图标设置对应的颜色。",
-        ["zh-tw"] = "為興奮劑圖示設定對應的顏色。",
+		["zh-tw"] = "為興奮劑圖示設定對應的顏色。",
 		ru = "Stimms Pickup Icon - Установливает соответствующие цвета для значков стимуляторов.",
 	},
 	show_pickup_color = {
 		en = "Show colorful pickup icons",
 		["zh-cn"] = "显示彩色拾取图标",
-        ["zh-tw"] = "顯示彩色拾取圖示",
+		["zh-tw"] = "顯示彩色拾取圖示",
 		ru = "Показать цветные значки поднимаемых",
 	},
 	show_team_panel_color = {
 		en = "Show colorful icons in team HUD",
 		["zh-cn"] = "在团队 HUD 中显示彩色图标",
-        ["zh-tw"] = "在團隊 HUD 中顯示彩色圖示",
+		["zh-tw"] = "在團隊 HUD 中顯示彩色圖示",
 		ru = "Показывать цветные значки в командном интерфейсе",
 	},
 	show_self_weapon_color = {
 		en = "Show colorful icons in weapon HUD",
 		["zh-cn"] = "在武器 HUD 中显示彩色图标",
-        ["zh-tw"] = "在武器 HUD 中顯示彩色圖示",
+		["zh-tw"] = "在武器 HUD 中顯示彩色圖示",
 		ru = "Показывать цветные иконки в интерфейсе оружия",
 	},
 	use_recolor_stimms_mod = {
 		en = "Use color settings of RecolorStimms mod",
 		["zh-cn"] = "使用 RecolorStimms 模组的颜色设置",
-        ["zh-tw"] = "使用 RecolorStimms 模組的顏色設定",
+		["zh-tw"] = "使用 RecolorStimms 模組的顏色設定",
 		ru = "Использовать настройки цвета мода «Recolor Stimms»",
 	},
 }
