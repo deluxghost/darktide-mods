1fa2ffe8c7981401d602e3a9ec1362a0955f7da3
SyuanTsai <carsun00@Gmail.com>
2025-03-31 11:19:36 +0800

    Added-zh-tw-translation

--- IME_Enable/scripts/mods/IME_Enable/IME_Enable_localization.lua
+++ IME_Enable/scripts/mods/IME_Enable/IME_Enable_localization.lua
@@ -2,11 +2,14 @@ return {
 	mod_name = {
 		en = "IME Enable",
 		["zh-cn"] = "启用输入法",
+		["zh-tw"] = "啟用輸入法",
 	},
 	mod_description = {
 		en = "Enable Windows Input Method for in-game text boxes. Useful for such as CJK languages players.\n\n" ..
 			"Some input methods themselves may not compatible with Darktide, which is not fixable by this mod.",
 		["zh-cn"] = "为游戏内文本框启用 Windows 输入法支持。适用于中日韩等语言的玩家。\n\n"..
 			"一些输入法自身不兼容暗潮，此模组无法解决此类问题。",
+		["zh-tw"] = "為遊戲內文字框啟用 Windows 輸入法支援。適用於中日韓等語言的玩家。\n\n" ..
+		"某些輸入法本身可能與《Darktide》不相容，此模組無法修正此類問題。",
 	},
 }
--- WhatTheLocalization/scripts/mods/WhatTheLocalization/WhatTheLocalization_localization.lua
+++ WhatTheLocalization/scripts/mods/WhatTheLocalization/WhatTheLocalization_localization.lua
@@ -3,55 +3,66 @@ return {
 		en = "What The Localization",
 		["zh-cn"] = "什么鬼翻译",
 		ru = "Что за локализация",
+		["zh-tw"] = "什麼鬼翻譯",
 	},
 	mod_description = {
 		en = "A community-driven localization fix framework and debug tool.",
 		["zh-cn"] = "一个由社区驱动的翻译修复框架和调试工具。",
 		ru = "What The Localization - Созданная сообществом платформа для исправления локализации и инструмент отладки.",
+		["zh-tw"] = "由社群推動的本地化修正框架與除錯工具。",
 	},
 	enable_debug_mode = {
 		en = "Debug mode",
 		["zh-cn"] = "调试模式",
 		ru = "Режим отладки",
+		["zh-tw"] = "除錯模式",
 	},
 	enable_debug_mode_description = {
 		en = "Show built-in text as localization keys.\nSome text are cached and requires the game to restart.",
 		["zh-cn"] = "以本地化键名显示内置文本。\n部分文本会被缓存，需要重启游戏才能生效。",
 		ru = "Показывать встроенный текст в качестве ключей локализации.\nНекоторый текст кэшируется и требует перезапуска игры.",
+		["zh-tw"] = "將內建文字顯示為本地化鍵值。\n部分文字會被快取，需要重新啟動遊戲才能生效。",
 	},
 	toggle_debug_mode = {
 		en = "Keybind: toggle debug mode",
 		["zh-cn"] = "快捷键：切换调试模式",
 		ru = "Привязка клавиш: переключение режима отладки",
+		["zh-tw"] = "快捷鍵：切換除錯模式",
 	},
 	message_debug_mode_on = {
 		en = "Localization debug mode: ON",
 		["zh-cn"] = "本地化调试模式：开",
 		ru = "Режим отладки локализации: ВКЛ.",
+		["zh-tw"] = "本地化除錯模式：開啟",
 	},
 	message_debug_mode_off = {
 		en = "Localization debug mode: OFF",
 		["zh-cn"] = "本地化调试模式：关",
 		ru = "Режим отладки локализации: ВЫКЛ.",
+		["zh-tw"] = "本地化除錯模式：關閉",
 	},
 	loc_command_description = {
 		en = "Output the value of a localization key.",
 		["zh-cn"] = "输出本地化键名对应的值。",
 		ru = "Выводит значение ключа локализации.",
+		["zh-tw"] = "輸出本地化鍵所對應的值。",
 	},
 	loc_command_missing_key = {
 		en = "/loc <localization_key>",
 		["zh-cn"] = "/loc <本地化键名>",
+		["zh-tw"] = "/loc <本地化鍵>",
 	},
 	loc_command_not_found = {
 		en = "Localization key not found",
 		["zh-cn"] = "本地化键名未找到",
 		ru = "Ключ локализации не найден",
+		["zh-tw"] = "找不到對應的本地化鍵",
 	},
 	loc_command_output_visualize = {
 		en = "Visualize whitespace characters",
 		["zh-cn"] = "可视化空白字符",
 		ru = "Показывать символы пробелов",
+		["zh-tw"] = "可視化空白字元",
 	},
 	loc_command_output_visualize_description = {
 		en = "Some whitespace characters in the output of /loc command will display as visible icons.\n\n" ..
@@ -65,6 +76,11 @@ return {
 			"\"\\n\" >> \n" ..
 			"\"\\r\" >> ",
 		ru = "Некоторые символы пробелов в выводе команды /loc будут отображаться в виде видимых значков.\n\n" ..
+			"\" \" >> \n" ..
+			"\"\\t\" >> \n" ..
+			"\"\\n\" >> \n" ..
+			"\"\\r\" >> ",
+		["zh-tw"] = "在 /loc 指令的輸出中，一些空白字元會以可見的圖示顯示。\n\n" ..
 			"\" \" >> \n" ..
 			"\"\\t\" >> \n" ..
 			"\"\\n\" >> \n" ..
