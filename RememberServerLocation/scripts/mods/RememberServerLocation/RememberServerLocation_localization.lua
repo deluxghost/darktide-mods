ba364d0a3177ee660ef383fb835ecf7c04c1fb74
Syuan <carsun00@gmail.com>
2025-04-01 17:05:40 +0800

    Added-zh-tw-translation (#63)
    * Added-zh-tw-translation
    
    * Fix Code review.
    
    * Fix

--- RememberServerLocation/scripts/mods/RememberServerLocation/RememberServerLocation_localization.lua
+++ RememberServerLocation/scripts/mods/RememberServerLocation/RememberServerLocation_localization.lua
@@ -3,10 +3,12 @@ return {
 		en = "Remember Server Location",
 		["zh-cn"] = "记住服务器位置",
 		ru = "Помни расположение сервера",
+		["zh-tw"] = "記住伺服器位置",
 	},
 	mod_description = {
 		en = "Remember matchmaking server location option.",
 		["zh-cn"] = "记住匹配服务器位置选项。",
 		ru = "Remember Server Location - Запоминает расположение сервера подбора Миссии в опциях.",
+		["zh-tw"] = "記住配對伺服器位置選項。",
 	},
 }
--- SimpleSpeedMeter/scripts/mods/SimpleSpeedMeter/SimpleSpeedMeter_localization.lua
+++ SimpleSpeedMeter/scripts/mods/SimpleSpeedMeter/SimpleSpeedMeter_localization.lua
@@ -2,10 +2,12 @@ return {
 	mod_name = {
 		en = "Simple Speed Meter",
 		["zh-cn"] = "简易速度计",
+		["zh-tw"] = "簡易速度計",
 	},
 	mod_description = {
 		en = "Adds a simple player speed meter widget.",
 		["zh-cn"] = "添加一个简易的玩家速度计组件。",
+		["zh-tw"] = "添加一個簡易的玩家速度計組件。",
 	},
 	speed_title = {
 		en = "SPD",
