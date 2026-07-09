c7a12467c64588c94dcbead0b762d0de681252a0
deluxghost <deluxghost@gmail.com>
2026-07-10 02:48:25 +0800

    IME_Enable: support imgui

--- IME_Enable/scripts/mods/IME_Enable/IME_Enable.lua
+++ IME_Enable/scripts/mods/IME_Enable/IME_Enable.lua
@@ -2,6 +2,7 @@ local mod = get_mod("IME_Enable")
 local UIWidget = require("scripts/managers/ui/ui_widget")
 
 local status = mod:persistent_table("status")
+local imgui_patch_mod = nil
 
 local function set_ime(value)
 	if not mod:is_enabled() then
@@ -9,6 +10,18 @@ local function set_ime(value)
 	end
 	value = value or false
 	Window.set_ime_enabled(value)
+end
+
+local function imgui_patch_wants_text_input()
+	if not imgui_patch_mod or not imgui_patch_mod.wants_text_input then
+		return false
+	end
+
+	return imgui_patch_mod.wants_text_input() == true
+end
+
+function mod.on_all_mods_loaded()
+	imgui_patch_mod = get_mod("ImguiPatch")
 end
 
 mod:hook_safe(UIWidget, "draw", function (widget, ui_renderer)
@@ -30,6 +43,6 @@ mod:hook_safe(UIWidget, "draw", function (widget, ui_renderer)
 end)
 
 function mod.update(dt)
-	set_ime(status.is_writing)
+	set_ime(status.is_writing or imgui_patch_wants_text_input())
 	status.is_writing = false
 end
